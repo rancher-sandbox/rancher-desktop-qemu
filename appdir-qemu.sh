ae6afdfdfccf18d6843542b8f29a9f697eb0530f
Jan Dubois <jan@jandubois.com>
2024-11-01 19:53:24 -0700

    Include version string in Linux artifact names

--- appdir-qemu.sh
+++ appdir-qemu.sh
@@ -46,7 +46,9 @@ set -ex
 [ -d "${1}" ] || error "Directory ${1} doesn't exist"
 
 appDir=$1
-dist="qemu-linux-x86_64"
+dist="qemu"
+[[ -n $VERSION ]] && dist="${dist}-${VERSION}"
+dist="${dist}-linux-x86_64"
 
 # Inspired on linuxdeployqt https://github.com/probonopd/linuxdeployqt/blob/master/tools/linuxdeployqt/excludelist.h
 # Linuxdeployqt is a tool created by probonopd, the AppImage creator
